a36f8d2eb7f8961a32cb4512ff4c88e8af5c2e3c
Vera Korzun <veraminich@gmail.com>
2020-12-16 23:21:22 +0100

    Added changed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './styles.css';
 import {refs} from './js/refs';
 import fetchImg from './js/fetchImg';
-import debounce from 'lodash.debounce';
 import {createMarkup} from './js/createMarkup';
 
 const {error} = require('@pnotify/core');
@@ -16,25 +15,41 @@ let page = 1;
 const loadImages = (query) => {
     return fetchImg(query, page, API_KEY)
         .then(data => {
-            createMarkup(data);
-            page+=1;
-            refs.btnLoad.style.display = "block";
+            if (data.length === 0) {
+                refs.btnLoad.style.display = "none";
+                const myError = error({
+                    text: "No images found. Please, try again.",
+                    maxTextHeight: null,
+                    delay: 3000,
+                     type: 'error'
+                });
+                console.log(myError);
+            } else {
+                createMarkup(data);
+                page+=1;
+                refs.btnLoad.style.display = "block";
+            }
         })
         .catch(error => console.log(error))
 };
 
 const getForm = (event) => {
     event.preventDefault();
-    console.log(event);
     if (refs.input.value === '') {
         refs.galleryList.innerHTML = '';
         refs.btnLoad.style.display = "none";
+        const myError = error({
+            text: "Nothing entered. Please, try again.",
+            maxTextHeight: null,
+            delay: 3000,
+             type: 'error'
+        });
+        console.log(myError);
     } else {
         page = 1;
         refs.galleryList.innerHTML = '';
        loadImages(refs.input.value);
     }
-    return false;
 }
 
 const loadMore = () => {
@@ -47,62 +62,8 @@ const loadMore = () => {
           })
           )
         .catch(error => console.log(error))
-       
     }
 }
 
-const runScript = (event) => {
-    event.preventDefault();
-    
-    console.log(event);
-    return false;
-}
-
-//refs.input.addEventListener('input', debounce(getForm, 500));
 refs.form.addEventListener('submit', getForm);
 refs.btnLoad.addEventListener('click', loadMore);
-
-// const myError = error({
-//           text: "Сountry not found. Please, try to enter again.",
-//           maxTextHeight: null,
-//           delay: 3000,
-//           type: 'error'
-//         });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let inputValue; 
-// //let page = 1;
-// //refs.btnLoad.style.display = "none";
-
-// const getFormSubmit = (event) => {
-//     event.preventDefault();
-//     refs.galleryList.innerHTML = '';
-//     inputValue = event.target.elements.query.value;
-//     if (inputValue.length > 1) {
-//         fetchImg(inputValue, page, API_KEY)
-//             .then(images =>{
-//                 console.log(images)
-//                 createMarkup(images)
-//                 //refs.btnLoad.style.display = "block";
-//             })
-//         .catch(err => console.log(err))
-//     }
-   
-// }
-
-// refs.form.addEventListener('input', getFormSubmit)
